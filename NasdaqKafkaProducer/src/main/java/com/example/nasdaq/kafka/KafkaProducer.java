2076e51ee74d005ea54876c75b6c6d4956e6673b
Everton Mendonca Jesus <evertonmj@gmail.com>
2024-09-24 23:33:41 -0300

    fix kafka producer

--- NasdaqKafkaProducer/src/main/java/com/example/nasdaq/kafka/KafkaProducer.java
+++ NasdaqKafkaProducer/src/main/java/com/example/nasdaq/kafka/KafkaProducer.java
@@ -1,11 +1,15 @@
 
 package com.example.nasdaq.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaProducer {
+    private static final Logger logger = LoggerFactory.getLogger(KafkaProducer.class);
+
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
@@ -13,6 +17,11 @@ public class KafkaProducer {
     }
 
     public void sendMessage(String topic, String message) {
-        kafkaTemplate.send(topic, message);
-    }
+        logger.info("Producing message to topic {}: {}", topic, message);
+        kafkaTemplate.send(topic, message)
+            .addCallback(
+                success -> logger.info("Message [{}] delivered with offset {}", message, success.getRecordMetadata().offset()),
+                failure -> logger.error("Unable to deliver message [{}]. Cause: {}", message, failure.getMessage())
+            );    
+            }
 }
--- NasdaqKafkaProducer/src/main/java/com/example/nasdaq/task/NasdaqDataProducerTask.java
+++ NasdaqKafkaProducer/src/main/java/com/example/nasdaq/task/NasdaqDataProducerTask.java
@@ -1,16 +1,16 @@
-
 package com.example.nasdaq.task;
 
 import com.example.nasdaq.kafka.KafkaProducer;
 import com.example.nasdaq.service.NasdaqService;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-public class NasdaqDataProducerTask {
-
+public class NasdaqDataProducerTask implements CommandLineRunner {
+    
     private final NasdaqService nasdaqService;
     private final KafkaProducer kafkaProducer;
 
@@ -25,14 +25,16 @@ public class NasdaqDataProducerTask {
         this.kafkaProducer = kafkaProducer;
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        fetchAndProduceData();
+    }
+
     @Scheduled(fixedRateString = "${fetch.interval.ms}")
     public void fetchAndProduceData() {
         try {
-            System.out.print("Producing message...")
             JsonNode data = nasdaqService.fetchData(stockSymbol);
             kafkaProducer.sendMessage(kafkaTopic, data.toString());
-            System.out.println(" > Message sent!")
-
         } catch (Exception e) {
             e.printStackTrace();
         }
